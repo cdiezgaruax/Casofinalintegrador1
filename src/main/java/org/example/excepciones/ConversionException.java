332ad618d0010a0cc9a1aca0db2637b9c9bba738
Carlos Garcia Diez-Madroñero <cdiezgar@myuax.com>
2025-02-25 13:30:57 +0100

    clase

---
+++ src/main/java/org/example/excepciones/ConversionException.java
@@ -0,0 +1,7 @@
+package org.example.excepciones;
+// File: src/main/java/org/example/ConversionException.java
+public class ConversionException extends Exception {
+    public ConversionException(String message) {
+        super(message);
+    }
+}
